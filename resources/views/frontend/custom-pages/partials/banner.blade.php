ae9b14d75b12737555c63980ca0bbba3f6828c3b
codebygourav <webclouddeveloper@gmail.com>
2026-07-18 21:39:32 +0530

    style: rename breadcrumb class to sub_breadcrumb and adjust policy section margin

--- resources/views/frontend/custom-pages/partials/banner.blade.php
+++ resources/views/frontend/custom-pages/partials/banner.blade.php
@@ -24,7 +24,7 @@
     <div class="ttf-page-banner__content">
         <div class="ttf-page-banner__crumbs">
              <h1>{{ $bannerTitle }}</h1>
-        <div class="breadcrumb">
+        <div class="sub_breadcrumb">
              <a href="{{ route('home') }}">{{ translate('Home') }}</a>
             <span>&raquo;</span>
             <span>{{ $breadcrumbLabel }}</span>
